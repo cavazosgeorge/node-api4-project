3ed294c948624a57f98694121f3b2433fd6b563a
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-29 01:42:37 -0400

    Updated users controller response

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,18 +1,14 @@
 const Users = require("../models/users");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 // METHOD(GET) GET ALL USERS => /API/V1/USERS
-exports.getUsers = async (req, res) => {
-  Users.find()
-    // .THEN => WE RECIEVED WHAT IS SUPPOSED TO BE THE CORRECT USER DATA
-    .then((users) => {
-      res.json(users);
-    })
-    // ERROR RESPONSE FOR DEVELOPER => 500 INTERNAL SERVER ERROR
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: "Error getting users",
-        err: err.message,
-      });
-    });
-};
+exports.getUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await Users.find();
+
+  res.status(200).json({
+    success: true,
+    results: users.length,
+    data: users,
+  });
+});
