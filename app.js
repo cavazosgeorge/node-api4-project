1671fe0c2026a9d8f3f3141a9e3724f7e6f1e411
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-28 22:07:19 -0400

    Created unhandled routes error handler

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
-
+const errorMiddleware = require("./middlewares/errors");
+const ErrorHandler = require("./utils/errorHandler");
 // SETTING UP CONFIG.ENV VARIABLES
 dotenv.config({
   path: "./config/config.env",
@@ -25,8 +26,11 @@ const users = require("./routes/users");
 app.use("/api/v1", users);
 
 // HANDLE UNHANDLED ROUTES
-
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
 // MIDDLEWARE => HANDLE ERRORS
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
